5eedad94f633912b30009acd82669a7b8f6560dd
Bluemooses <jacob.brenke@gmail.com>
2023-04-22 18:54:08 -0500

    Add Post and post route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
+import { api } from "~/utils/api";
 import { SignInButton, useUser, SignOutButton } from "@clerk/nextjs";
 
 const Home: NextPage = () => {
   const user = useUser();
 
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -16,7 +17,12 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
           {!user.isSignedIn && <SignInButton />}
-          {!!user.isSignedIn && <SignOutButton />}
+          {user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post: any) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
