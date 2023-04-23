1c4ee9b1210da82e02ac27d71ee1d31480e9de29
Bluemooses <jacob.brenke@gmail.com>
2023-04-22 22:43:44 -0500

    Add err handling for post route

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 const filterUserForClient = (user: User) => {
   return {
     id: user.id,
@@ -22,9 +23,20 @@ export const postsRouter = createTRPCRouter({
       })
     ).map(filterUserForClient);
 
-    return posts.map((post) => ({
-      post,
-      author: users.find((user) => user.id === post.authorId),
-    }));
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+      }
+
+      return {
+        post,
+        author,
+      };
+    });
   }),
 });
